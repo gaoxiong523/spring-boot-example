9ef272381c18a79d1b6289c37b88991727243b5b
200506725@qq.com <gaojunchen520>
2018-09-22 17:00:39 +0800

    使用webjars 加载静态资源

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ hint: See the 'Note about fast-forwards' in 'git push --help' for details.
 
 本文来自 microcosm1994 的CSDN 博客 ，全文地址请点击：https://blog.csdn.net/qq_39081974/article/details/78590907?utm_source=copy 
 
+使用webjars 加载静态资源
+``<link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link th:href="@{/webjars/bootstrap/4.1.3/css/bootstrap.css}" rel="stylesheet">``
--- springboot-crud/src/main/java/com/gaoxiong/springboot/config/MyMvcConfig.java
+++ springboot-crud/src/main/java/com/gaoxiong/springboot/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.gaoxiong.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,4 +20,17 @@ public class MyMvcConfig implements WebMvcConfigurer {
         //添加视图映射
         registry.addViewController("/login").setViewName("login");
     }
+
+    @Bean//两种配置视图解析器的方法
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer(){
+            @Override
+            public void addViewControllers ( ViewControllerRegistry registry ) {
+                registry.addViewController("/").setViewName("/index");
+            }
+
+        };
+        return webMvcConfigurer;
+    }
 }
+
